849b24540ecfaac1225c858c207b659f50a50a43
rush.33 <rushadmahmed@gmail.com>
2023-06-15 12:27:12 +0530

    update

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -14,25 +14,10 @@ import {
   SiRedux,
 } from "react-icons/si";
 
-const skillsData = [
-  { icon: <SiJavascript />, text: "Javascript", skillColor: "yellow-400" },
-  { icon: <SiReact />, text: "React", skillColor: "blue-400" },
-  { icon: <SiReact />, text: "React Native", skillColor: "blue-600" },
-  { icon: <SiTailwindcss />, text: "Tailwind CSS", skillColor: "blue-400" },
-  { icon: <SiNextdotjs />, text: "Next JS", skillColor: "black" },
-  { icon: <SiMongodb />, text: "Mongo DB", skillColor: "green-500" },
-  { icon: <SiRedux />, text: "Redux", skillColor: "purple-500" },
-  { icon: <SiNodedotjs />, text: "Node JS", skillColor: "green-600" },
-  { icon: <SiCss3 />, text: "CSS", skillColor: "blue-600" },
-  { icon: <SiGit />, text: "Git", skillColor: "orange-500" },
-  { icon: <SiHtml5 />, text: "HTML", skillColor: "orange-600" },
-  { icon: <SiExpress />, text: "Express JS", skillColor: "indigo-600" },
-];
-
-const SkillItem = ({ icon, text, skillColor }) => (
+const SkillItem = ({ icon, text, color }) => (
   <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
     <div className="flex flex-col justify-center items-center">
-      <div className={`mx-auto text-5xl text-${skillColor}`}>{icon}</div>
+      <div className={`mx-auto text-5xl text-${color}`}>{icon}</div>
       <p className="font-semibold my-1 cursor-default">{text}</p>
     </div>
   </div>
@@ -40,7 +25,7 @@ const SkillItem = ({ icon, text, skillColor }) => (
 
 const Skills = () => {
   return (
-    <div id="skills" className="w-4/5 py-24 mx-auto">
+    <div id="skills" className="w-4/5 py-32 mx-auto">
       <div className="mx-auto flex flex-col flex-start">
         <p className="text-xl font-medium tracking-widest uppercase text-black">
           <RoughNotation
@@ -54,15 +39,31 @@ const Skills = () => {
           </RoughNotation>
         </p>
         <h2>Experience</h2>
-        <div className="mt-12 p-5 gap-12 sm:p-0 flex flex-wrap justify-center w-full mx-auto">
-          {skillsData.map((skill, index) => (
-            <SkillItem
-              key={index}
-              icon={skill.icon}
-              text={skill.text}
-              skillColor={skill.skillColor}
-            />
-          ))}
+        <div className="mt-12 p-5 gap-6 sm:gap-12 sm:p-0 flex flex-wrap justify-center w-full mx-auto">
+          <SkillItem
+            icon={<SiJavascript />}
+            text="Javascript"
+            color="yellow-400"
+          />
+          <SkillItem icon={<SiReact />} text="React" color="blue-400" />
+          <SkillItem icon={<SiReact />} text="React Native" color="blue-600" />
+          <SkillItem
+            icon={<SiTailwindcss />}
+            text="Tailwind CSS"
+            color="blue-400"
+          />
+          <SkillItem icon={<SiNextdotjs />} text="Next JS" color="black" />
+          <SkillItem icon={<SiMongodb />} text="Mongo DB" color="green-500" />
+          <SkillItem icon={<SiRedux />} text="Redux" color="purple-500" />
+          <SkillItem icon={<SiNodedotjs />} text="Node JS" color="green-600" />
+          <SkillItem icon={<SiCss3 />} text="CSS" color="blue-600" />
+          <SkillItem icon={<SiGit />} text="Git" color="orange-500" />
+          <SkillItem icon={<SiHtml5 />} text="HTML" color="orange-600" />
+          <SkillItem
+            icon={<SiExpress />}
+            text="Express JS"
+            color="indigo-700"
+          />
         </div>
       </div>
     </div>
